b22742f3fd7686b1d8bd152f37fb2dce749ec1da
xj修行者 <vip115333347shequ@Hotmail.com>
2017-12-16 16:17:44 +0800

    First upload

---
+++ common/footer.php
@@ -0,0 +1,6 @@
+<script src="https://cdn.bootcss.com/mdui/0.3.0/js/mdui.min.js"></script>
+<footer>
+<div class="copyright">&copy; <a rel="nofollow" href="https://www.feking.org/archives/sq.html">矢量实验室</a>丨风栖社区</div>
+</footer>
+ </body>
+  </html>
